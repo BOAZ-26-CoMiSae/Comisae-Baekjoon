20fc06f7239c1d794f80ae044509bbd79758d005
davidcho0701 <davidcho0701@gmail.com>
2026-04-02 15:22:49 +0900

    Add 10250.java and 10818.java for week2

---
+++ 조성빈/week2/10250.java
@@ -0,0 +1,32 @@
+package 조성빈.week2;
+
+public class 10250 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int H = sc.nextInt(); 
+            int W = sc.nextInt(); 
+            int N = sc.nextInt(); 
+
+            int floor; 
+            int roomNum; 
+
+            if (N % H == 0) {
+                
+                floor = H;
+                roomNum = N / H;
+            } else {
+            
+                floor = N % H;
+                roomNum = (N / H) + 1;
+            }
+
+            
+            System.out.printf("%d%02d\n", floor, roomNum);
+        }
+    }
+}
