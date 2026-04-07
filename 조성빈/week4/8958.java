182906acf118576eac3517acf26714791ce02d5e
davidcho0701 <davidcho0701@gmail.com>
2026-04-07 14:44:31 +0900

    8958

---
+++ 조성빈/week4/8958.java
@@ -0,0 +1,28 @@
+package 조성빈.week4;
+import java.util.Scanner;
+
+public class 8958 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(i=0; i=t; i++) {
+            String s = sc.next();
+
+            int totalScore = 0;
+            int currentZeroCount = 0;
+
+            for(int j=0; j<s.length(); j++) {
+                if(s.charAt(j) == '0') {
+                    currentCorrectCount++;
+                    totalScore += currentCorrectCount;
+                } else {
+                    currentCorrectCount = 0; //Point : 연속되고 있는 O 이 끝나면 리셋하여 연속추가할당 스톱시켜주기
+                }
+            }
+            System.out.println(totalScore);
+        }
+        sc.close;
+    }
+}
