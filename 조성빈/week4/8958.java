82304205d3273fcf5e258401b68f5251d5e11161
davidcho0701 <davidcho0701@gmail.com>
2026-04-07 14:45:33 +0900

    8958

--- 조성빈/week4/8958.java
+++ 조성빈/week4/8958.java
@@ -7,22 +7,22 @@ public class 8958 {
 
         int t = sc.nextInt();
 
-        for(i=0; i=t; i++) {
+        for(int i=0; i<t; i++) {
             String s = sc.next();
 
             int totalScore = 0;
-            int currentZeroCount = 0;
+            int currentOCount = 0;
 
             for(int j=0; j<s.length(); j++) {
-                if(s.charAt(j) == '0') {
-                    currentCorrectCount++;
-                    totalScore += currentCorrectCount;
+                if(s.charAt(j) == 'O') {
+                    currentOCount++;
+                    totalScore += currentOCount;
                 } else {
-                    currentCorrectCount = 0; //Point : 연속되고 있는 O 이 끝나면 리셋하여 연속추가할당 스톱시켜주기
+                    currentOCount = 0; //Point : 연속되고 있는 O 이 끝나면 리셋하여 연속추가할당 스톱시켜주기
                 }
             }
             System.out.println(totalScore);
         }
-        sc.close;
+        sc.close();
     }
 }
