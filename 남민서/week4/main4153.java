151ec0576a60f7284292baac5522d2af9cfa0834
Minseo Nam <minseo031364@gmail.com>
2026-04-08 17:22:13 +0900

    남민서 코테 week4 4153번 직각삼각형 문제

--- 남민서/week4/main4153.java
+++ 남민서/week4/main4153.java
@@ -1,6 +1,46 @@
 // 직각삼각형 
 package 남민서.week4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class main4153 {
-    
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0 && c == 0) break;
+
+            int max = Math.max(a, Math.max(b, c));
+
+            if (max == a) {
+                if (b*b + c*c == a*a) {
+                    System.out.println("right");
+                } else {
+                    System.out.println("wrong");
+                }
+            } else if (max == b) {
+                if (a*a + c*c == b*b) {
+                    System.out.println("right");
+                } else {
+                    System.out.println("wrong");
+                }
+            } else {
+                if (a*a + b*b == c*c) {
+                    System.out.println("right");
+                } else {
+                    System.out.println("wrong");
+                }
+            }
+        }
+    }
 }
