e7a5739ce58f73f83b6f3fc45344f015a8af71f5
davidcho0701 <davidcho0701@gmail.com>
2026-05-14 16:41:33 +0900

    Lv0 풀이

--- 조성빈/week7/Lv0_옹알이.py
+++ 조성빈/week7/Lv0_옹알이.py
@@ -1,14 +1,15 @@
-def solition(babbliing):
+def solution(babbling):
     answer = 0
-    can speak = ["aya", "ye", "woo", "ma"]
+    can_speak = ["aya", "ye", "woo", "ma"]  # 말할 수 있는 발음 리스트
 
-    for word in babbling:
-        for speakable inn can_speak:
-
-            if speakable in word:
-            word = word.replace(speakable, " ")
-        
-        if word.strip() == "":
+    for word in babbling: #옹알이 단어 하나씩 검사
+        for speakable in can_speak: 
+            if speakable in word: 
+                word = word.replace(speakable, " ") # 말할 수 있는 발음을 공백으로 대체
+        if word.strip() == "": 
             answer += 1
-
+            
     return answer
+
+
+
