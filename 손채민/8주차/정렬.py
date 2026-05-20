7e81e14f58d1fe8f9fc471db16c0a24c9aa17044
molcham <130418659+molcham@users.noreply.github.com>
2026-05-20 20:18:48 +0900

    feat: 채민8주차 업로드

---
+++ 손채민/8주차/정렬.py
@@ -0,0 +1,13 @@
+def solution(array, commands):
+    # for문에서 결과값 넣을 list 생성
+    result = []
+
+    for i in range(len(commands)):
+        pass
+
+    answer = [0] * len(result)
+
+    for i in range(len(result)):
+        answer[i] = result[i]
+
+    return answer
